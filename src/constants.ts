38091004be61b9d086963c61b0c61de36dca5797
Sleeeeeepy <adjustedmin@gmail.com>
2024-01-31 12:04:38 +0900

    fix: missing configurations

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
+// domain settings
+export const DOMAIN_NAME = process.env.DOMAIN_NAME;
+export const SERVER_PORT = 3000;
+
 // jwt settings
 export const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -5,7 +9,17 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 export const DBMS_NAME = 'mysql';
 export const DBSERVER_HOST = 'localhost';
 export const DBSERVER_PORT = 3306;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_USERNAME = process.env.DB_USERNAME;
-
+export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_NAME = 'tetris';
+
+// mailing settings
+export const MAIL_HOST = process.env.MAIL_HOST;
+export const MAIL_PORT = process.env.MAIL_PORT;
+export const MAIL_USER = process.env.MAIL_USER;
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+export const MAIL_DEFAULTS_FROM = process.env.MAIL_DEFAULTS_FROM;
+
+// mailing queue settings
+export const MAIL_REDIS_HOST = 'localhost';
+export const MAIL_REDIS_PORT = 6379;
